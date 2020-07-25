43df49fa752a3455f7aa832fcde2c6e181924dd0
Itamar Amith <itamaram@gmail.com>
2020-07-25 10:38:15 +1000

    First pass

---
+++ background.js
@@ -0,0 +1,44 @@
+// ID to manage the context menu entry
+var cmid;
+var cm_clickHandler = function (data) {
+    chrome.tabs.create({
+        url: Base64.decode(data.selectionText)
+    })
+};
+
+function isUrl(s){
+    return !!new RegExp('^https?:\\/\\/').test(s)
+}
+
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+chrome.runtime.onMessage.addListener(function (msg) {
+    console.log('invoked')
+
+    if (msg.request !== 'onSelectionChange')
+        return;
+
+    var selection = Base64.decode(msg.selection);
+
+    console.log({selection: selection, raw: msg.selection})
+
+    if (selection == '' || !isUrl(selection)) {
+        // Remove the context menu entry
+        if (cmid != null) {
+            chrome.contextMenus.remove(cmid);
+            cmid = null; // Invalidate entry now to avoid race conditions
+        } // else: No contextmenu ID, so nothing to remove
+    } else { // Add/update context menu entry
+        var options = {
+            title: 'Go to ' + selection,
+            contexts: ['selection'],
+            onclick: cm_clickHandler
+        };
+        if (cmid != null) {
+            chrome.contextMenus.update(cmid, options);
+        } else {
+            // Create new menu, and remember the ID
+            cmid = chrome.contextMenus.create(options);
+        }
+    }
+});
